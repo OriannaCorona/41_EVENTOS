b66ae462300d9d1965128bf0d57416cada826873
OriannaCorona <oriannalievanos12@gmail.com>
2016-10-26 13:24:17 -0500

    agrego ultimos cambios del ejercicio de trello, me falto solo que se pudieran guardar las tarjetas de la lista

--- trello/js/main.js
+++ trello/js/main.js
@@ -16,20 +16,43 @@ function desplegarBtnGuardar() {
   	var contenedor = document.getElementById("contenedor");
   	contenedor.replaceChild(caja, nombreLista);	  	
 //Para guardar una lista nueva
-	var inputLista = document.createElement("input");
+	var listaHecha = document.createElement("DIV");
+	var linkTarjeta = document.createElement("a");
 	btnGuardar.onclick = function crearListaNueva(e) {
 		var viejoDiv = e.target.parentElement;
 		var tituloLista = document.createTextNode(inputNuevaLista.value); 
-		var listaHecha = document.createElement("DIV");
 		listaHecha.appendChild(tituloLista);
 	  	listaHecha.setAttribute("class", "listaHecha");
 	  	viejoDiv.parentElement.replaceChild(listaHecha, viejoDiv);
-	  	inputLista.placeholder = "Añadir una tarjeta..."; 
-	  	inputLista.className = "inputLista";
-		listaHecha.appendChild(inputLista);
+	  	linkTarjeta.appendChild(document.createTextNode("Añadir una tarjeta...")); 
+	  	linkTarjeta.className = "inputLista";
+		listaHecha.appendChild(linkTarjeta);
 	  	document.getElementById("contenedorDos").appendChild(listaHecha);
 	  	contenedor.appendChild(nombreLista);  	 	
 	}
+//Para crear una tajeta nueva
+	var inputNuevaTarjeta = document.createElement("input");
+	var btnGuardarTarjeta = document.createElement("button");
+	linkTarjeta.onclick = function crearTarjeta() {
+		var cajaTarjeta = document.createElement("DIV");
+		inputNuevaTarjeta.className = "inputNuevaTarjeta";
+		var btnGuardarTarjeta = document.createElement("button");
+		btnGuardarTarjeta.className = "btnGuardarTarjeta";
+		var textoBoton = document.createTextNode("Guardar tarjeta");
+		btnGuardarTarjeta.appendChild(textoBoton);
+		var tache = document.createElement("SPAN");
+  		tache.className = "glyphicon glyphicon-remove";
+  		cajaTarjeta.appendChild(inputNuevaTarjeta);
+  		cajaTarjeta.appendChild(btnGuardarTarjeta);
+  		cajaTarjeta.appendChild(tache);
+  		listaHecha.replaceChild(cajaTarjeta, linkTarjeta);	
+	}
 //Para guardar una tajeta nueva
-	
+	btnGuardarTarjeta.onclick = function guardarTarjeta() {
+		var tarjetaHecha = document.createElement("DIV");
+		var tituloTarjeta = document.createTextNode(inputNuevaTarjeta.value); 
+	  	tarjetaHecha.setAttribute("class", "tarjetaHecha");
+	  	tarjetaHecha.appendChild(tituloTarjeta);
+	  	listaHecha.appendChild(tarjetaHecha); 	
+	}
 }
